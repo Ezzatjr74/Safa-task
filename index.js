0fc5a4c1c5a97e06494b65f079aff2ec90e027b9
Ezzatjr74 <omare6412@gmail.com>
2022-09-26 05:16:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+$('.ourwallet').hover(
+    function(){
+        $('.wallet-dropdown').toggle('.drop')
+    }
+)
+
+$('.ournotify').hover(
+    function(){
+        $('.notify-dropdown').toggle('.drop')
+    }
+)
+
+$('.ourlangue').hover(
+    function(){
+        $('.langue-dropdown').toggle('.drop')
+    }
+)
+$('.ourprofile').hover(
+    function(){
+        $('.profile-dropdown').toggle('.drop')
+
+    }
+)
+
+$('#closemine').hover(
+    function(){
+        $('.big-servies').toggle('.drop')
+    }
+)
+
+
+
